d2bb6b135216dc9f418518fd9e6ffb8058a0cb20
Mack Ianni <mackianni@Macks-MacBook-Pro.local>
2024-04-27 22:30:01 -0600

    working

--- player1/main.py
+++ player1/main.py
@@ -2,33 +2,28 @@ import socket
 
 def main():
     host = '10.0.0.224'  # Server IP
-    port = 65433         # Server port
+    port = 65435         # Server port
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         client_socket.connect((host, port))
-    except ConnectionError:
-        print("Unable to connect to the server.")
-        return
+        print("Connected to server at {}:{}".format(host, port))
 
-    print("Connected to server at {}:{}".format(host, port))
-    server_message = client_socket.recv(1024).decode('utf-8')
-    print(server_message)  # Welcome message
+        server_message = client_socket.recv(1024).decode('utf-8')
+        print(server_message)  # Welcome message
 
-    try:
         while True:
-            # Receive game board or game updates
             server_response = client_socket.recv(2048).decode('utf-8')
-            if server_response:
-                print(server_response)
-            else:
+            if not server_response:
                 break  # Server has closed the connection
+            print(server_response)
 
-            # Send a move
             move = input("Enter your move (row col): ")
             if move.lower() == 'exit':
                 break
             client_socket.send(move.encode('utf-8'))
+    except ConnectionError:
+        print("Unable to connect to the server.")
     except Exception as e:
         print("An error occurred:", e)
     finally:
